5a32025bb695696ab0b84c3098ebd4e4e17c08bb
Sachhyam Shakya <sachhyamcrook@gmail.com>
2021-03-19 14:05:13 +0545

    integration of firebase messaging

--- easytravel/lib/screens/userprofile.dart
+++ easytravel/lib/screens/userprofile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 class UserProfile extends StatefulWidget {
   @override
@@ -6,12 +7,43 @@ class UserProfile extends StatefulWidget {
 }
 
 class _UserProfileState extends State<UserProfile> {
+  FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  String messageTitle = "title";
+  String notificationAlert = "alert";
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _firebaseMessaging.getToken().then((token) {
+      print(token);
+    });
+    _firebaseMessaging.configure(
+      onMessage: (message) async {
+        setState(() {
+          messageTitle = message["notification"]["title"];
+          notificationAlert = "New Notification Alert";
+        });
+      },
+      onResume: (message) async {
+        setState(() {
+          messageTitle = message["notification"]["title"];
+          notificationAlert = "Application opened from Notification";
+        });
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
-        body: Center(child: Text('User profile')),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Text(messageTitle), Text(notificationAlert)],
+          ),
+        ),
       ),
     );
   }
