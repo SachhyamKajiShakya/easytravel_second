d8f0357f74a2be52190a46114f4a7394bd74a6f3
Sachhyam Shakya <sachhyamcrook@gmail.com>
2021-04-04 20:50:24 +0545

    Notification completed

--- easytravel/lib/screens/bookings/confirmlongbooking.dart
+++ easytravel/lib/screens/bookings/confirmlongbooking.dart
@@ -30,7 +30,6 @@ class ConfirmLongBooking extends StatelessWidget {
                 );
               }
               return ListView.builder(
-                scrollDirection: Axis.horizontal,
                 itemCount: 1,
                 itemBuilder: (context, index) {
                   print(snapshot.data);
@@ -46,64 +45,62 @@ class ConfirmLongBooking extends StatelessWidget {
                               bottomRight: Radius.circular(10)),
                         ),
                         padding: EdgeInsets.all(20),
-                        child: SingleChildScrollView(
-                          child: Column(
-                            children: [
-                              _buildData('Customer Name',
-                                  snapshot.data["customer_name"], snapshot),
-                              SizedBox(height: 20),
-                              _buildData('Customer Contact',
-                                  snapshot.data["customer_contact"], snapshot),
-                              SizedBox(height: 20),
-                              _buildData('Driver Name',
-                                  snapshot.data["driver_name"], snapshot),
-                              SizedBox(height: 20),
-                              _buildData('Driver Contact',
-                                  snapshot.data["driver_contact"], snapshot),
-                              SizedBox(height: 20),
-                              _buildData(
-                                  'Vehicle',
-                                  snapshot.data["vehicle_brand"] +
-                                      ' ' +
-                                      snapshot.data["vehicle_model"],
-                                  snapshot),
-                              SizedBox(height: 20),
-                              _buildData('Pickup Date',
-                                  snapshot.data["pick_up_date"], snapshot),
-                              SizedBox(height: 20),
-                              _buildData('Pickup Time',
-                                  snapshot.data["pick_up_time"], snapshot),
-                              SizedBox(height: 20),
-                              _buildData(
-                                  'Pickup Location',
-                                  snapshot.data["pick_up_province"].toString() +
-                                      ',' +
-                                      snapshot.data["pick_up_district"] +
-                                      ' ' +
-                                      snapshot.data["pick_up_city"] +
-                                      ' ' +
-                                      snapshot.data["pick_up_street"],
-                                  snapshot),
-                              SizedBox(height: 20),
-                              _buildData(
-                                  'Destination Location',
-                                  snapshot.data["destination_province"]
-                                          .toString() +
-                                      ',' +
-                                      snapshot.data["destination_district"] +
-                                      ' ' +
-                                      snapshot.data["destination_city"] +
-                                      ' ' +
-                                      snapshot.data["destination_street"],
-                                  snapshot),
-                              SizedBox(height: 20),
-                              _buildData(
-                                  'Booked For',
-                                  snapshot.data["number_of_days"].toString() +
-                                      ' days',
-                                  snapshot),
-                            ],
-                          ),
+                        child: Column(
+                          children: [
+                            _buildData('Customer Name',
+                                snapshot.data["customer_name"], snapshot),
+                            SizedBox(height: 20),
+                            _buildData('Customer Contact',
+                                snapshot.data["customer_contact"], snapshot),
+                            SizedBox(height: 20),
+                            _buildData('Driver Name',
+                                snapshot.data["driver_name"], snapshot),
+                            SizedBox(height: 20),
+                            _buildData('Driver Contact',
+                                snapshot.data["driver_contact"], snapshot),
+                            SizedBox(height: 20),
+                            _buildData(
+                                'Vehicle',
+                                snapshot.data["vehicle_brand"] +
+                                    ' ' +
+                                    snapshot.data["vehicle_model"],
+                                snapshot),
+                            SizedBox(height: 20),
+                            _buildData('Pickup Date',
+                                snapshot.data["pick_up_date"], snapshot),
+                            SizedBox(height: 20),
+                            _buildData('Pickup Time',
+                                snapshot.data["pick_up_time"], snapshot),
+                            SizedBox(height: 20),
+                            _buildData(
+                                'Pickup Location',
+                                snapshot.data["pick_up_province"].toString() +
+                                    ',' +
+                                    snapshot.data["pick_up_district"] +
+                                    ' ' +
+                                    snapshot.data["pick_up_city"] +
+                                    ' ' +
+                                    snapshot.data["pick_up_street"],
+                                snapshot),
+                            SizedBox(height: 20),
+                            _buildData(
+                                'Destination Location',
+                                snapshot.data["destination_province"]
+                                        .toString() +
+                                    ',' +
+                                    snapshot.data["destination_district"] +
+                                    ' ' +
+                                    snapshot.data["destination_city"] +
+                                    ' ' +
+                                    snapshot.data["destination_street"],
+                                snapshot),
+                            SizedBox(height: 20),
+                            _buildData(
+                                'Booked For',
+                                snapshot.data["number_of_days"].toString() +
+                                    ' days',
+                                snapshot),
+                          ],
                         ),
                       ),
                       SizedBox(height: 30),
